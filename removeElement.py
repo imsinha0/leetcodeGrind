314c6356db6b0f0e12b8e2bc32f1e8b469950ad4
ishaan <ishaan.mohan.sinha@gmail.com>
2024-11-05 22:53:56 -0500

    removeElement

---
+++ removeElement.py
@@ -0,0 +1,25 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+
+        i = 0 #index of nums
+        count = 0
+        #we want to loop through and send the val elements to the other side
+        for _ in range(len(nums)):
+            if(nums[i] == val):
+                nums.append(nums.pop(i))
+            else:
+                i += 1
+            count += 1
+        return i, nums
+
+
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [3,2,2,3]
+    val = 3
+    print(solution.removeElement(nums, val))
+
+
