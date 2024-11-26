16144445564e4f3d3d847a10e04a99751a652df6
ishaan <ishaan.mohan.sinha@gmail.com>
2024-11-25 22:24:12 -0500

    get random O(1)

---
+++ insertDeleteGetRandomO(1).py
@@ -0,0 +1,42 @@
+'''
+class RandomizedSet:
+
+    def __init__(self):
+        self.
+
+
+    def insert(self, val: int) -> bool:
+        if(val not in set):
+
+
+    def remove(self, val: int) -> bool:
+
+    def getRandom(self) -> int:
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
+
+'''
+
+import random
+'''
+start = 100
+
+for days in range(100000):
+    y = random.randint(0,1)
+    if(y == 0):
+        start = start/2 +  0.5* start /2
+    else:
+        start *= start/2 + 1/2 * start * 1.7
+
+print(start)
+'''
+
+
+
+
+
+
